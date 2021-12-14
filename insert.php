46e1fa43203ce8a72d20b3cd9abcaae1c37622a0
emderma12 <93205579+emderma12@users.noreply.github.com>
2021-12-14 16:06:45 -0300

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,12 @@
+<?php 
+ 
+$nome = $_POST['nome'];
+$Email = $_POST['E-mail'];
+$idade = $_POST['idade'];
+
+echo $nome."-".$Email."-".$idade;
+
+
+
+
+ ?>
---
+++ questao04.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+	<title>questao 4</title>
+</head>
+<body>
+	<form action="insert.php" method="POST">
+	
+<p> nome: <input type="text" name="nome"></p>
+<p> E-mail: <input type="email" name="E-mail"> </p>
+<p> idade: <input type="number" name="idade"> </p>
+
+	<button type="submit"> inserir dados </button>
+	</form>
+</body>
+</html>
